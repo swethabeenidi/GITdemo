690ae6f72dffa643c36b62d45f5a985253200397
swethabeenidi <swethadepi123@gmail.com>
2021-01-31 14:27:40 +0100

    commiting the files

---
+++ src/main/java/yeildlab/App.java
@@ -0,0 +1,13 @@
+package yeildlab;
+
+/**
+ * RUN THE APPLICATIO!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "RUN THE APPLICATION" );
+    }
+}
